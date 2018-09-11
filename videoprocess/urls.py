64f5440146c2e140ea2ed5a20a039676791ecec7
sherlockcooper <sherlockcooper@sina.com>
2018-09-11 11:23:04 +0800

    user register

--- videoprocess/urls.py
+++ videoprocess/urls.py
@@ -7,5 +7,10 @@ urlpatterns=[
     path('mjpeg/', views.mjpeg),
 	path('welcome/', views.welcome),
 	path('play/', views.playSaiki),
-	re_path('play/(?P<fileName>.+)', views.playVideo)
+	re_path('play/(?P<fileName>.+)', views.playVideo),
+    path('login/', views.log_in),
+    path('logout/', views.log_out),
+    path('manage/', views.manage_user),
+    path('delete', views.delete_user),
+    path('add', views.add_user)
 ]
--- videoprocess/views.py
+++ videoprocess/views.py
@@ -12,7 +12,7 @@ import time
 
 
 # Create your views here.
-cap=cv2.VideoCapture('No6.mp4')
+cap=cv2.VideoCapture(0)
 #cap = cv2.VideoCapture(0)
 
 def start_camera():
@@ -88,11 +88,69 @@ def show_frame(request):
         return response
 def sign_up(request):
     if request.method=='GET':
+
         return render(request, 'signup.html')
     elif request.method=='POST':
         usr_name=request.POST.get('username')
         usr_password=request.POST.get('password')
-        usr_email=request.POST.get('email')
-        User.objects.create_user(username=usr_name, password=usr_password)
+        usr_confirm=request.POST.get('confirm')
 
-        return redirect('/video/refresh')
+        if User.objects.filter(username=usr_name):
+            instruction = '用户已存在，请直接登录'
+            return render(request, 'signup.html', {'instruction': instruction})
+        # if authenticate(username=usr_name):
+        #     instruction='用户已存在，请直接登录'
+        #     return render(request, 'signup.html', {'instruction':instruction})
+        if usr_password==usr_confirm:
+            User.objects.create_user(username=usr_name, password=usr_password)
+            return redirect('/video/login')
+        else:
+            instruction='请重新确认密码'
+            return render(request, 'signup.html', {'instruction':instruction})
+
+def log_in(request):
+    if request.method=='GET':
+        return render(request, 'login.html')
+    elif request.method=='POST':
+        usr_name=request.POST.get('username')
+        usr_password=request.POST.get('password')
+
+        if User.objects.filter(username=usr_name):
+            possible_user = User.objects.get(username=usr_name)
+            if possible_user.check_password(usr_password):
+                request.session['logstate']='logged'
+                request.session['usrname']=usr_name
+                return redirect('/video/refresh')
+            else:
+                instruction='密码不正确，请重新登录'
+                return render(request, 'login.html', {'instruction':instruction})
+        else:
+            instruction='用户不存在，请先注册'
+            return render(request, 'login.html', {'instruction':instruction})
+
+def log_out(request):
+    request.session.flush()
+    return redirect('/video/login')
+
+def manage_user(request):
+    if request.method=='GET':
+        show_users=[]
+        all_users=User.objects.all()
+        for user in all_users:
+            if not user.is_superuser:
+                show_users.append(user)
+        return render(request, 'manage.html', {'show_users': show_users})
+
+def delete_user(request):
+    if request.method=='POST':
+        for each_user in User.objects.all():
+            if request.POST.get(str(each_user.id)):
+                each_user.delete()
+        return redirect('/video/manage')
+
+def add_user(request):
+    if request.method=='POST':
+        usr_name=request.POST.get('username')
+        usr_password=request.POST.get('password')
+        User.objects.create_user(username=usr_name, password=usr_password)
+        return redirect('/video/manage')
